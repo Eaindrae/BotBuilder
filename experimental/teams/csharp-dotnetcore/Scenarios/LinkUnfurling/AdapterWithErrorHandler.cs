e62a1fd67dc29ec0329a4fa175516c75f353996e
Dave Taniguchi <daveta@microsoft.com>
2019-10-15 09:41:11 -0700

    Add C# client

--- experimental/teams/csharp-dotnetcore/Scenarios/LinkUnfurling/AdapterWithErrorHandler.cs
+++ experimental/teams/csharp-dotnetcore/Scenarios/LinkUnfurling/AdapterWithErrorHandler.cs
@@ -1,16 +1,29 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Bot.Builder;
 using Microsoft.Bot.Builder.Integration.AspNet.Core;
+using Microsoft.Bot.Connector.Authentication;
+using Microsoft.Bot.Schema;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Microsoft.Rest.Serialization;
+using Newtonsoft.Json;
 
 namespace Microsoft.BotBuilderSamples
 {
     public class AdapterWithErrorHandler : BotFrameworkHttpAdapter
     {
-        public AdapterWithErrorHandler(IConfiguration configuration, ILogger<BotFrameworkHttpAdapter> logger)
-            : base(configuration, logger)
+        public AdapterWithErrorHandler(ILogger<BotFrameworkHttpAdapter> logger)
+            : base(new SimpleCredentialProvider(null, null), null, logger)
         {
             OnTurnError = async (turnContext, exception) =>
             {
