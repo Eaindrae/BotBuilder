5a4e511379f3ca8414446ec069dd60f5b75760ed
stevengum <14935595+stevengum@users.noreply.github.com>
2019-10-19 15:07:46 -0700

    js echobot with streaming, restify and express

--- samples/javascript_nodejs/02.echo-bot/app.js
+++ samples/javascript_nodejs/02.echo-bot/app.js
@@ -1,31 +1,18 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+const express = require('express');
 const dotenv = require('dotenv');
 const path = require('path');
-const restify = require('restify');
 
 // Import required bot services.
 // See https://aka.ms/bot-services to learn more about the different parts of a bot.
 const { ActivityTypes, BotFrameworkAdapter } = require('botbuilder');
-
-// This bot's main dialog.
 const { EchoBot } = require('./bot');
 
-// Import required bot configuration.
 const ENV_FILE = path.join(__dirname, '.env');
 dotenv.config({ path: ENV_FILE });
 
-// Create HTTP server, configure the Restify Server to add support for streaming.
-const server = restify.createServer({ handleUpgrades: true });
-server.listen(process.env.port || process.env.PORT || 3978, () => {
-    console.log(`\n${ server.name } listening to ${ server.url }`);
-    console.log(`\nGet Bot Framework Emulator: https://aka.ms/botframework-emulator`);
-    console.log(`\nTo talk to your bot, open the emulator select "Open Bot"`);
-});
-
-// Create adapter.
-// See https://aka.ms/about-bot-adapter to learn more about how bots work.
 const adapter = new BotFrameworkAdapter({
     appId: process.env.MicrosoftAppId,
     appPassword: process.env.MicrosoftAppPassword,
@@ -56,19 +43,44 @@ adapter.onTurnError = async (context, error) => {
     await context.sendActivity(`To continue to run this bot, please fix the bot source code.`);
 };
 
-// Create the main dialog.
 const bot = new EchoBot();
 
-// Listen for incoming requests.
-server.post('/api/messages', (req, res) => {
+const app = express();
+
+app.post('/api/messages', (req, res) => {
     adapter.processActivity(req, res, async (context) => {
         // Route to main dialog.
         await bot.run(context);
     });
 });
 
-// Listen for GET requests to the same route to accept Upgrade requests for Streaming.
-server.get('/api/messages', (req, res) => {
+// Express doesn't have built-in upgrade support, so we need to store the
+// HTTP Server created by app.listen().
+const server = app.listen(process.env.port || process.env.PORT || 3978);
+console.log(`\nListening on port ${ server.address().port } at ${ server.address().address }`);
+
+// To enable streaming on the bot, the app needs to listen for the 'upgrade' event from the HTTP server.
+// Register an event handler for the 'upgrade' event: https://nodejs.org/api/http.html#http_event_upgrade_1
+// Modify the request by creating a method that returns the socket and head, mirroring Restify
+server.on('upgrade', (req, socket, head) => {
+    // These socket.destroy() calls emit a Server error that closes the Node process.
+    if (req.method !== 'GET') {
+        socket.destroy(new Error(`Unexpected method received for upgrade: ${ req.method }. Only "GET" is accepted.`));
+        return;
+    }
+
+    if (req.url !== '/api/messages' && req.url !== '/api/messages/') {
+        socket.destroy(new Error(`Unexpected URL on upgrade request: ${ req.url }`));
+        return;
+    }
+
+    // Ugly shim
+    const res = {
+        claimUpgrade() {
+            return { socket, head };
+        }
+    };
+
     adapter.processActivity(req, res, async (context) => {
         // Route to main dialog.
         await bot.run(context);
