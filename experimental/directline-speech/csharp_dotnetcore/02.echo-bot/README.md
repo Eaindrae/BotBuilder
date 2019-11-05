1a94739f4930a245f0f97a8ca25c167dc1f150fb
Steven Gum <14935595+stevengum@users.noreply.github.com>
2019-11-05 11:46:38 -0800

    Revert "[Streaming] Update DLS Node.js Echo bot and delete DLS .netcore samples (#1905)"
    This reverts commit 651f4053ed5f99332454fe057f8af832150e56cf.

--- experimental/directline-speech/csharp_dotnetcore/02.echo-bot/README.md
+++ experimental/directline-speech/csharp_dotnetcore/02.echo-bot/README.md
@@ -1,48 +1,49 @@
-# echo-bot
+﻿# EchoBot
 
-Bot Framework v4 echo bot sample
+Bot Framework v4 echo bot sample.
 
 This bot has been created using [Bot Framework](https://dev.botframework.com), it shows how to create a simple bot that accepts input from the user and echoes it back.
 
+To allow this bot to use Direct Line Speech, these changes were made to the original EchoBot sample:
+1. Use the WebSocketEnabledHttpAdapter in the startup.cs file.
+2. Enabled WebSockets in your application by calling app.UseWebSockets() in the startup.cs file. Note this call must be made BEFORE calling app.UseMVC().
+3. Adding HttpGet (in addition to HttpPost for other channels) to the Controller method in BotController.cs.
+4. Use the "Speak" helper method to send an Activity with the correct Speak field set to have Direct Line Speech turn the message into speech.
+
 ## Prerequisites
 
-- [Node.js](https://nodejs.org) version 10.14 or higher
+- [.NET Core SDK](https://dotnet.microsoft.com/download) version 2.1
 
-    ```bash
-    # determine node version
-    node --version
-    ```
-
-- In order to use the preview version of the [botbuilder-streaming-extensions](https://botbuilder.myget.org/feed/botbuilder-v4-js-daily/package/npm/botbuilder-streaming-extensions) library required to use Direct Line Speech, run the following terminal commands from the root dir of the bot:
-1. npm config set registry https://botbuilder.myget.org/F/botbuilder-v4-js-daily/npm/
-2. npm install
-3. npm config set registry https://registry.npmjs.org/
+  ```bash
+  # determine dotnet version
+  dotnet --version
+  ```
 
 ## To try this sample
 
 - Clone the repository
 
     ```bash
-    git clone https://github.com/microsoft/botbuilder-samples.git
+    git clone https://github.com/Microsoft/botbuilder-samples.git
     ```
 
-- In a terminal, navigate to `samples/javascript_nodejs/02.echo-bot`
+- In a terminal, navigate to `samples/csharp_dotnetcore/02.echo-bot`
+- Run the bot from a terminal or from Visual Studio, choose option A or B.
 
-    ```bash
-    cd samples/javascript_nodejs/02.echo-bot
-    ```
+  A) From a terminal
 
-- Install modules
+  ```bash
+  # run the bot
+  dotnet run
+  ```
 
-    ```bash
-    npm install
-    ```
+  B) Or from Visual Studio
 
-- Start the bot
-
-    ```bash
-    npm start
-    ```
+  - Launch Visual Studio
+  - File -> Open -> Project/Solution
+  - Navigate to `samples/csharp_dotnetcore/02.echo-bot` folder
+  - Select `EchoBot.csproj` file
+  - Press `F5` to run the project
 
 ## Testing the bot using Bot Framework Emulator
 
@@ -67,6 +68,7 @@ To learn more about deploying a bot to Azure, see [Deploy your bot to Azure](htt
 - [Activity processing](https://docs.microsoft.com/en-us/azure/bot-service/bot-builder-concept-activity-processing?view=azure-bot-service-4.0)
 - [Azure Bot Service Introduction](https://docs.microsoft.com/azure/bot-service/bot-service-overview-introduction?view=azure-bot-service-4.0)
 - [Azure Bot Service Documentation](https://docs.microsoft.com/azure/bot-service/?view=azure-bot-service-4.0)
+- [.NET Core CLI tools](https://docs.microsoft.com/en-us/dotnet/core/tools/?tabs=netcore2x)
 - [Azure CLI](https://docs.microsoft.com/cli/azure/?view=azure-cli-latest)
 - [Azure Portal](https://portal.azure.com)
 - [Language Understanding using LUIS](https://docs.microsoft.com/en-us/azure/cognitive-services/luis/)
--- experimental/directline-speech/javascript_nodejs/02.echo-bot/index.js
+++ experimental/directline-speech/javascript_nodejs/02.echo-bot/index.js
@@ -1,72 +1,68 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+const dotenv = require('dotenv');
 const path = require('path');
 const restify = require('restify');
 
 // Import required bot services.
 // See https://aka.ms/bot-services to learn more about the different parts of a bot.
 const { BotFrameworkAdapter } = require('botbuilder');
+const { BotFrameworkStreamingAdapter } = require('botbuilder-streaming-extensions');
 
 // This bot's main dialog.
 const { EchoBot } = require('./bot');
 
-// Note: Ensure you have a .env file and include MicrosoftAppId and MicrosoftAppPassword to use Direct Line Speech
+// Import required bot configuration.
 const ENV_FILE = path.join(__dirname, '.env');
-require('dotenv').config({ path: ENV_FILE });
+dotenv.config({ path: ENV_FILE });
 
-// Create HTTP server and configure the Server to handle for Upgrade requests.
+// Create HTTP server
 const server = restify.createServer({ handleUpgrades: true });
 server.listen(process.env.port || process.env.PORT || 3978, () => {
     console.log(`\n${ server.name } listening to ${ server.url }`);
-    console.log('\nGet Bot Framework Emulator: https://aka.ms/botframework-emulator');
-    console.log('\nTo talk to your bot, open the emulator select "Open Bot"');
+    console.log(`\nGet Bot Framework Emulator: https://aka.ms/botframework-emulator`);
+    console.log(`\nTo talk to your bot, open the emulator select "Open Bot"`);
 });
 
 // Create adapter.
 // See https://aka.ms/about-bot-adapter to learn more about how bots work.
 const adapter = new BotFrameworkAdapter({
     appId: process.env.MicrosoftAppId,
-    appPassword: process.env.MicrosoftAppPassword,
-    enableWebSockets: true
+    appPassword: process.env.MicrosoftAppPassword
 });
 
 // Catch-all for errors.
 adapter.onTurnError = async (context, error) => {
     // This check writes out errors to console log .vs. app insights.
-    // NOTE: In production environment, you should consider logging this to Azure
-    //       application insights.
-    console.error(`\n [onTurnError] unhandled error: ${ error }`);
-
-    // Send a trace activity, which will be displayed in Bot Framework Emulator
-    await context.sendTraceActivity(
-        'OnTurnError Trace',
-        `${ error }`,
-        'https://www.botframework.com/schemas/error',
-        'TurnError'
-    );
-
+    console.error(`\n [onTurnError]: ${ error }`);
     // Send a message to the user
-    await context.sendActivity('The bot encounted an error or bug.');
-    await context.sendActivity('To continue to run this bot, please fix the bot source code.');
+    await context.sendActivity(`Oops. Something went wrong!`);
 };
 
 // Create the main dialog.
-const myBot = new EchoBot();
+const bot = new EchoBot();
 
 // Listen for incoming requests.
 server.post('/api/messages', (req, res) => {
     adapter.processActivity(req, res, async (context) => {
         // Route to main dialog.
-        await myBot.run(context);
+        await bot.run(context);
     });
 });
 
-// Listen for GET requests to the same route to accept Upgrade requests for Streaming.
-server.get('/api/messages', (req, res) => {
-    adapter.processActivity(req, res, async (context) => {
-        // After connecting via WebSocket, run this logic for every request sent over
-        // the WebSocket connection.
-        await myBot.run(context);
+// If a GET is received defer to the streaming extensions package.
+// The adapter will handle creation of a new streaming
+// connection and route all messages over through the passed in
+// bot.
+// A MiddleWare or MiddleWareSet may be passed in as an optional
+// third argument to the adapter, and will be executed
+// in the same manner as a typical bot.
+server.get('/api/messages', function upgradeRoute(req, res) {
+    const streamingAdapter = new BotFrameworkStreamingAdapter(bot);
+    streamingAdapter.connectWebSocket(req, res, { 
+        appId: process.env.MicrosoftAppId,
+        appPassword: process.env.MicrosoftAppPassword,
+        channelService: process.env.ChannelService,
     });
 });
